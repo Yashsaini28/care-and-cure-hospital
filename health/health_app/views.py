f8a57037743ab99d9b0cfc776e70e9c2033c3d62
Yashsaini28 <49223762+Yashsaini28@users.noreply.github.com>
2021-05-29 16:15:38 +0530

    Update views.py

--- health/health_app/views.py
+++ health/health_app/views.py
@@ -361,11 +361,22 @@ def doctordisease(request):
     else:
         g=dfy['nutrient_1'].iloc[0]
         i=dfy['Nutrient_2'].iloc[0]
-    if len(li)>0:
+    if li[0]:
         items1=li[0]
+    else:
+        items1=''
+    if li[1]:
         items2=li[1]
-        items3=li[2]
-        items4=li[3]
+    else:
+        items2=''
+    if li[2]:
+        items3=li[0]
+    else:
+        items3=''
+    if li[3]:
+        items4=li[0]
+    else:
+        items4=''
     #print(li)
     
     #print(a)
